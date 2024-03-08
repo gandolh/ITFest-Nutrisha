fec72d9d4fde124eec294fb560ff90cc25f9b7d7
gandolh <29356778+gandolh@users.noreply.github.com>
2024-03-08 23:22:03 +0200

    Updated header

--- FE/src/shared/HeaderMegaMenu.tsx
+++ FE/src/shared/HeaderMegaMenu.tsx
@@ -96,58 +96,10 @@ import {
               <a href="#" className={classes.link}>
                 Home
               </a>
-              <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-                <HoverCard.Target>
-                  <a href="#" className={classes.link}>
-                    <Center inline>
-                      <Box component="span" mr={5}>
-                        Features
-                      </Box>
-                      <IconChevronDown
-                        style={{ width: rem(16), height: rem(16) }}
-                        color={theme.colors.blue[6]}
-                      />
-                    </Center>
-                  </a>
-                </HoverCard.Target>
-  
-                <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
-                  <Group justify="space-between" px="md">
-                    <Text fw={500}>Features</Text>
-                    <Anchor href="#" fz="xs">
-                      View all
-                    </Anchor>
-                  </Group>
-  
-                  <Divider my="sm" />
-  
-                  <SimpleGrid cols={2} spacing={0}>
-                    {links}
-                  </SimpleGrid>
-  
-                  <div className={classes.dropdownFooter}>
-                    <Group justify="space-between">
-                      <div>
-                        <Text fw={500} fz="sm">
-                          Get started
-                        </Text>
-                        <Text size="xs" c="dimmed">
-                          Their food sources have decreased, and their numbers
-                        </Text>
-                      </div>
-                      <Button variant="default">Get started</Button>
-                    </Group>
-                  </div>
-                </HoverCard.Dropdown>
-              </HoverCard>
               <a href="#" className={classes.link}>
-                Learn
-              </a>
-              <a href="#" className={classes.link}>
-                Academy
+                MyPage
               </a>
             </Group>
-  
             <Group visibleFrom="sm">
               <Button variant="default">Log in</Button>
               <Button>Sign up</Button>
@@ -172,24 +124,11 @@ import {
             <a href="#" className={classes.link}>
               Home
             </a>
-            <UnstyledButton className={classes.link} onClick={toggleLinks}>
-              <Center inline>
-                <Box component="span" mr={5}>
-                  Features
-                </Box>
-                <IconChevronDown
-                  style={{ width: rem(16), height: rem(16) }}
-                  color={theme.colors.blue[6]}
-                />
-              </Center>
-            </UnstyledButton>
             <Collapse in={linksOpened}>{links}</Collapse>
             <a href="#" className={classes.link}>
-              Learn
+              My Page
             </a>
-            <a href="#" className={classes.link}>
-              Academy
-            </a>
+         
   
             <Divider my="sm" />
   
