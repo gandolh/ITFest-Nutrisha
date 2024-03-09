80fa64a4b5a0e2aae01272062839baccdd6321dc
gandolh <29356778+gandolh@users.noreply.github.com>
2024-03-09 13:53:26 +0200

    fixed minor bug when toggle theme powered up power button

--- FE/src/components/shared/PowerButton.tsx
+++ FE/src/components/shared/PowerButton.tsx
@@ -11,54 +11,55 @@ type PowerButtonProps = {
 
 const PowerButton = ({ authenticatedUser }: PowerButtonProps) => {
     const navigate = useNavigate();
-    
+
     const HandleLogout = () => {
         localStorage.removeItem('authenticatedUser');
         navigate('/');
     }
 
     return (
+        <>
+            <ToggleThemeComp />
+            <Popover width={300} trapFocus position="bottom" withArrow shadow="md">
+                <Popover.Target>
+                    <div className='flex gap-4'>
+                        <UnstyledButton className={classes.user}>
+                            <Group>
+                                <Avatar
+                                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
+                                    radius="xl"
+                                />
 
-        <Popover width={300} trapFocus position="bottom" withArrow shadow="md">
-            <Popover.Target>
-                <div className='flex gap-4'>
-                    <ToggleThemeComp/>
-                    <UnstyledButton className={classes.user}>
-                        <Group>
-                            <Avatar
-                                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
-                                radius="xl"
-                            />
+                                <div style={{ flex: 1 }}>
+                                    <Text size="sm" fw={500}>
+                                        {authenticatedUser.firstName + ' ' + authenticatedUser.lastName}
+                                    </Text>
 
-                            <div style={{ flex: 1 }}>
-                                <Text size="sm" fw={500}>
-                                    {authenticatedUser.firstName + ' ' + authenticatedUser.lastName}
-                                </Text>
+                                    <Text c="dimmed" size="xs">
+                                        {authenticatedUser.email}
+                                    </Text>
+                                </div>
 
-                                <Text c="dimmed" size="xs">
-                                    {authenticatedUser.email}
-                                </Text>
-                            </div>
-
-                            <IconChevronDown style={{ width: rem(14), height: rem(14) }} stroke={1.5} />
-                        </Group>
-                    </UnstyledButton>
-                </div>
-            </Popover.Target>
-            <Popover.Dropdown>
-                <Stack gap={0}>
-                    <Link className='w-full bg-white' to="/info">
-                        My info
-                    </Link>
-                    <Divider my={'xs'}/>
-                    <Link className='w-full bg-white' to="#">
-                        Others 🙂
-                    </Link>
-                    <Divider my={'xs'}/>
-                    <Button variant="default" onClick={HandleLogout}>Log out</Button>
-                </Stack>
-            </Popover.Dropdown>
-        </Popover>
+                                <IconChevronDown style={{ width: rem(14), height: rem(14) }} stroke={1.5} />
+                            </Group>
+                        </UnstyledButton>
+                    </div>
+                </Popover.Target>
+                <Popover.Dropdown>
+                    <Stack gap={0}>
+                        <Link className='w-full bg-white' to="/info">
+                            My info
+                        </Link>
+                        <Divider my={'xs'} />
+                        <Link className='w-full bg-white' to="#">
+                            Others 🙂
+                        </Link>
+                        <Divider my={'xs'} />
+                        <Button variant="default" onClick={HandleLogout}>Log out</Button>
+                    </Stack>
+                </Popover.Dropdown>
+            </Popover>
+        </>
 
     );
 }
