028c8bb3b6664c8431351a2d74196b096e66cac8
Tudor Mateian <tudor.mateian@ibm.com>
2024-03-09 22:44:26 +0200

    generateRecipes

--- OllamaAPI/app.py
+++ OllamaAPI/app.py
@@ -17,83 +17,100 @@ ollama_model = 'llama2'
 
 @app.route('/recipes/generate', methods=['GET'])
 def call_ollama():
+    count = 0
+    total = recipes.count_documents({})
     for recipe in recipes.find():
-        title = recipe.get('title')
-
-        generated_object = ollama.generate(model='gemma', prompt="Give me details about the " + title + " recipe using the following template:\nDescription:\n<long description>\nIngredients:\n- <number> <unit of measure>\n- <number> <just the ingredient>\nPreparing steps:\n1. <step description>\n2. <step description>\nNutritional values:\n- <number> calorie\n- <number> protein\n- <number> fat\n- <number> carbohydrate")
-
-        response = generated_object.get('response')
-
-        response = response.replace('\n', '')
-
-        description_string = response[response.find('**Description:**') + 16 : response.find('**Ingredients:**')]
-        ingredients_string = response[response.find('**Ingredients:**') + 16 : response.find('**Preparing steps:**')]
-        steps_string = response[response.find('**Preparing steps:**') + 20 : response.find('**Nutritional values:**')]
-        nutritional_values_string = response[response.find('**Nutritional values:**') + 23 :]
-
-        description = description_string
-
-        ingredients = ingredients_string.split('- ')
-        ingredients = ingredients[1:]
-
-        steps = []
-        step = ""
-        for i in range(len(steps_string)):
-            if steps_string[i].isdigit() and steps_string[i + 1] == '.':
-                steps.append(step)
-                step = ""
-            step += steps_string[i]
-        steps.append(step)
-        steps = steps[1:]
-
-        nutritional_values = nutritional_values_string.split('- ')
-        nutritional_numbers = []
-        number = 0
-        for x in nutritional_values:
-            for char in x:
-                if char.isdigit():
-                    number = number * 10 + int(char)
-                else:
-                    nutritional_numbers.append(number)
-                    number = 0
-                    break
-
-        ingredients_json = []
-        for ingredient in ingredients:
-            ingredients_temp = ingredient.split(' ')
-            if ingredients_temp[1].find('cup') != -1 or ingredients_temp[1].find('tsp') != -1 or ingredients_temp[1].find('tbsp') != -1 or ingredients_temp[1].find('oz') != -1 or ingredients_temp[1].find('lb') != -1 or ingredients_temp[1].find('pound') != -1 or ingredients_temp[1].find('teaspoon') != -1 or ingredients_temp[1].find('tablespoon') != -1:
-                ingredients_json.append({"amount": ingredients_temp[0] + ' ' + ingredients_temp[1], "name": ' '.join(ingredients_temp[2 : ])})
-            elif ingredients_temp[0].isdigit():
-                ingredients_json.append({"amount": ingredients_temp[0], "name": ' '.join(ingredients_temp[1 : ])})
-            else:
-                ingredients_json.append({"amount": "", "name": ' '.join(ingredients_temp[0 : ])})
-
-        steps_json = []
-        for step in steps:
+        # If the recipe does not have a description, skip it
+        if recipe.get('description') != None:
+            count += 1
+            title = recipe.get('title')
+            print("Skipped: " + title)
+            print(str(count) + " / " + str(total))
+            continue
+        else:
+            count += 1
+            title = recipe.get('title')
+        
+            generated_object = ollama.generate(model='gemma', prompt="Give me details about the " + title + " recipe using the following template:\nDescription:\n<long description>\nIngredients:\n- <number> <unit of measure>\n- <number> <just the ingredient>\nPreparing steps:\n1. <step description>\n2. <step description>\nNutritional values:\n- <number> calorie\n- <number> protein\n- <number> fat\n- <number> carbohydrate")
+        
+            response = generated_object.get('response')
+        
+            response = response.replace('\n', '')
+        
+            description_string = response[response.find('**Description:**') + 16 : response.find('**Ingredients:**')]
+            ingredients_string = response[response.find('**Ingredients:**') + 16 : response.find('**Preparing steps:**')]
+            steps_string = response[response.find('**Preparing steps:**') + 20 : response.find('**Nutritional values:**')]
+            nutritional_values_string = response[response.find('**Nutritional values:**') + 23 :]
+        
+            description = description_string
+        
+            ingredients = ingredients_string.split('- ')
+            ingredients = ingredients[1:]
+        
+            steps = []
+            step = ""
+            for i in range(len(steps_string)):
+                if steps_string[i].isdigit() and steps_string[i + 1] == '.':
+                    steps.append(step)
+                    step = ""
+                step += steps_string[i]
+            steps.append(step)
+            steps = steps[1:]
+        
+            nutritional_values = nutritional_values_string.split('- ')
+            nutritional_numbers = []
             number = 0
-            i = 0
-            while i < len(step):
-                if step[i].isdigit():
-                    number = number * 10 + int(step[i])
+            for x in nutritional_values:
+                for char in x:
+                    if char.isdigit():
+                        number = number * 10 + int(char)
+                    else:
+                        nutritional_numbers.append(number)
+                        number = 0
+                        break
+        
+            ingredients_json = []
+            for ingredient in ingredients:
+                ingredients_temp = ingredient.split(' ')
+                print("Ingredients: " + str(ingredients_temp))
+                if len(ingredients_temp) == 1:
+                    ingredients_json.append({"amount": "", "name": ingredients_temp[0]})
+                elif ingredients_temp[1].find('cup') != -1 or ingredients_temp[1].find('tsp') != -1 or ingredients_temp[1].find('tbsp') != -1 or ingredients_temp[1].find('oz') != -1 or ingredients_temp[1].find('lb') != -1 or ingredients_temp[1].find('pound') != -1 or ingredients_temp[1].find('teaspoon') != -1 or ingredients_temp[1].find('tablespoon') != -1:
+                    ingredients_json.append({"amount": ingredients_temp[0] + ' ' + ingredients_temp[1], "name": ' '.join(ingredients_temp[2 : ])})
+                elif ingredients_temp[0].isdigit():
+                    ingredients_json.append({"amount": ingredients_temp[0], "name": ' '.join(ingredients_temp[1 : ])})
                 else:
-                    break
-                i += 1
-            desc = step[i + 2 : ]
-            steps_json.append({"order": number, "description": desc})
+                    ingredients_json.append({"amount": "", "name": ' '.join(ingredients_temp[0 : ])})
+        
+            steps_json = []
+            for step in steps:
+                number = 0
+                i = 0
+                while i < len(step):
+                    if step[i].isdigit():
+                        number = number * 10 + int(step[i])
+                    else:
+                        break
+                    i += 1
+                desc = step[i + 2 : ]
+                steps_json.append({"order": number, "description": desc})
+        
+            recipe = {
+                "description": description,
+                "ingredients": ingredients_json,
+                "steps": steps_json,
+                "calories": nutritional_numbers[0],
+                "protein": nutritional_numbers[1],
+                "fat": nutritional_numbers[2],
+                "carbohydrate": nutritional_numbers[3]
+            }
+        
+            recipes.update_one({"title": title}, {"$set": recipe})
+            print("Updated: " + title)
+            print(str(count) + " / " + str(total))
+    
+    return "Updated " + str(count) + " out of " + str(total) + " recipes!"
 
-        recipe = {
-            "description": description,
-            "ingredients": ingredients_json,
-            "steps": steps_json,
-            "calories": nutritional_numbers[0],
-            "protein": nutritional_numbers[1],
-            "fat": nutritional_numbers[2],
-            "carbohydrate": nutritional_numbers[3]
-        }
-
-        recipes.update_one({"title": title}, {"$set": recipe})
-
-    return "Success!"
 
 
 def add_recipe(title):
