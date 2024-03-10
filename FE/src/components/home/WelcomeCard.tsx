81b03f33d04caf6807341d53c04389a9030d9987
Tudor Mateian <tudor.mateian@ibm.com>
2024-03-10 06:21:38 +0200

    s m o l  c h a n g e s

--- FE/src/components/home/WelcomeCard.tsx
+++ FE/src/components/home/WelcomeCard.tsx
@@ -1,18 +1,20 @@
 import { Card, Text, Title} from '@mantine/core';
+import { useAuthContext } from '../auth/AuthContext';
 
 const WelcomeCard = () => {
   const currentTime = new Date().getHours();
+  const { curentUser } = useAuthContext();
   let greeting;
   let background_image;
   
   if (currentTime < 13) {
-    greeting = "Good morning";
+    greeting = "Good Morning";
     background_image = "Morning.webp";
   } else if (currentTime < 18) {
-    greeting = "Good afternoon";
+    greeting = "Good Afternoon";
     background_image = "Noon.webp";
   } else {
-    greeting = "Good evening";
+    greeting = "Good Evening";
     background_image = "Evening.webp";
   }
   
@@ -28,7 +30,7 @@ const WelcomeCard = () => {
       >
         <h1 className='fw-bold text-white absolute bottom-[5%] mt-100 left-0 w-full text-center'
           style={{fontSize: 'calc(2rem * var(--mantine-scale))'}}>
-           {greeting}
+           {greeting}{curentUser?.firstName === undefined ? "!" : ", " + curentUser?.firstName + "!"}
         </h1>
       </Card>
 
